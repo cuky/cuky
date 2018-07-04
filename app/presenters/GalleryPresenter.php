1087083e77370357675ff92a59014c1865a31729
cuky <lukas.wiener@gmail.com>
2018-07-05 01:44:17 +0200

    gallery picture update

--- app/presenters/GalleryPresenter.php
+++ app/presenters/GalleryPresenter.php
@@ -7,4 +7,8 @@ use Nette;
 
 class GalleryPresenter extends Nette\Application\UI\Presenter
 {
+    public function renderDefault()
+    {
+        $this->template->gallery=["jon-tyson-533596-unsplash.jpg","casey-horner-487085-unsplash.jpg"];
+    }
 }
