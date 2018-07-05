30ca6b70741f81db0f7f9e954bedaca1a1bcb423
cuky <lukas.wiener@gmail.com>
2018-07-05 15:35:20 +0200

    added blog

---
+++ app/presenters/BlogPresenter.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Presenters;
+
+use Nette;
+
+
+class BlogPresenter extends Nette\Application\UI\Presenter
+{
+}
--- app/presenters/GalleryPresenter.php
+++ app/presenters/GalleryPresenter.php
@@ -9,6 +9,6 @@ class GalleryPresenter extends Nette\Application\UI\Presenter
 {
     public function renderDefault()
     {
-        $this->template->gallery=["jon-tyson-533596-unsplash.jpg","casey-horner-487085-unsplash.jpg"];
+        $this->template->gallery=["jon-tyson-533596-unsplash.jpg","casey-horner-487085-unsplash.jpg","thomas-kelley-64329-unsplash.jpg","wil-stewart-18242-unsplash.jpg","kalen-emsley-99666-unsplash.jpg","elke-karin-lugert-125493-unsplash.jpg"];
     }
 }
